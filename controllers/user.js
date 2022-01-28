ff91667c61651ab5b0b7e2334d094445a91f53ca
Eduardo Ansa <eduardoansa@gmail.com>
2022-01-27 22:18:41 -0300

    backend listo protitipo

---
+++ controllers/user.js
@@ -0,0 +1,49 @@
+const {response, request} = require('express')
+
+const usuariosGet = (req = request, res = response) => {
+
+    const {q, nombre="no name", page, limit} = req.query;
+
+    res.json({
+        msg:'get API -- controlador',
+        q,
+        nombre,
+        page,
+        limit
+
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const {id} = req.params;
+
+    res.json({
+        msg:'put API -- controlador',
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg:'post API -- controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg:'delete API -- controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete
+}
